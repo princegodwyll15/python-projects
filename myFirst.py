45eb23f9d717a4c38a1add29e691d5ef71ccd2b1
princegodwyll15 <godwyllprince@mail.com>
2024-12-26 19:27:45 +0000

    update

--- myFirst.py
+++ myFirst.py
@@ -2,7 +2,12 @@ import tkinter as tk
 myWindow = tk.Tk()
 myWindow.title("My First GUI Application")
 myWindow.geometry("600x600")
-label = tk.Label(text="My First GUI Application", font=("Arial", 18))
+myWindow.configure(bg="white")
+
+label = tk.Label(myWindow, text="My First GUI Application", font=("Arial", 18))
 label.pack()
+
+button = tk.Button(myWindow, text=("Click Me"), bg="red", font=("Arial", 15))
+button.pack(side=tk.TOP, padx= 1, pady=2)
 
 myWindow.mainloop()
